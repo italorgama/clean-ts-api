c2ef55c6f3995b36b83d776f50832b95592138a7
Italo Gama <itrgama@gmail.com>
2022-10-08 18:34:06 -0300

    feat: connect to mongodb before integration test

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/signup', (req, res) => {
-    res.json({ ok: 'ok' })
+    console.log(req.body)
+    res.json(req.body)
   })
 }
